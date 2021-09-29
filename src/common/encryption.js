ba0015136cb72f9bb526d4a4a11919a4c7002f60
Hell Vision <2856782165@qq.com>
2021-09-30 00:16:30 +0800

    【功能新增】加密算法bcrypt

---
+++ src/common/encryption.js
@@ -0,0 +1,44 @@
+const _bcrypt = require('bcrypt');
+const saltRounds = 5;
+
+module.exports = _bcrypt
+
+module.exports = bcrypt = {
+    saltRounds: saltRounds,
+    // 加密
+    hash: async function (password) {
+        return await _bcrypt.hash(password, this.saltRounds)
+    },
+    // 同步加密，兼容明文密码
+    hashSync: function (password) {
+        return password.indexOf('$2b') !== 0 && password.indexOf('$2a') !== 0 ? _bcrypt.hashSync(password, this.saltRounds) : password
+    },
+    // 比对
+    compare: async function(password, hash) {
+        hash = this.hashSync(hash)
+        return await _bcrypt.compare(password, hash)
+    },
+    /**
+     * 加密字符串并处理
+     * @param password {String}
+     * @param callback {Function}
+     * @param option {Object}
+     */
+    haseDel: function (password, callback = function (err, hash) {}, option) {
+        _bcrypt.hash(password, this.saltRounds, option ? (err, hash) => {callback(err, hash, option)} : callback)
+    },
+    /**
+     * 比对字符串并处理
+     * @param password {String}
+     * @param hash {String}
+     * @param callback {Function}
+     * @param option {Object}
+     */
+    compareDel: function (password, hash, callback = function (err, result) {}, option) {
+        hash = this.hashSync(hash)
+        _bcrypt.compare(password, hash, option ? (err, result) => {callback(err, result, option)} : callback)
+    },
+    setSaltRounds: function (saltRounds) {
+        this.saltRounds = saltRounds
+    }
+}
